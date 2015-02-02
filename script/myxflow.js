da8fec299c444f8a5e682e8851666306a4ceafe8
Stefan Lemme <stefan.lemme@dfki.de>
2015-02-02 19:10:38 +0100

    heightfield finally working

--- script/myxflow.js
+++ script/myxflow.js
@@ -1,8 +1,10 @@
 
+XML3D.options.setValue("renderer-faceculling", "back");
+
 
 Xflow.registerOperator("xflow.vertexNormal", {
 	outputs: [	{type: 'float3', name: 'normal', customAlloc: true} ],
-	params:  [  {type: 'float3', source: 'position' },
+	params:  [  {type: 'float3', source: 'position', array: true },
 				{type: 'int', source: 'index' } ],
 	alloc: function(sizes, position, index)
 	{
@@ -12,64 +14,69 @@ Xflow.registerOperator("xflow.vertexNormal", {
 		var vl = position.length;
 		var il = index.length;
 		
-		// for each vertex i in VertexList v
-		for (var i=0; i < vl; i+=3) {
-			
-			// n ← Zero Vector
-			var n = [0, 0, 0];
-			
-			// for each triangle j that shares ith vertex {
-			for (var j=0; j < il; j+=3) {
-				if ((index[j  ] != i) && (index[j+1] != i) && (index[j+2] != i))
-					continue;
-				
-				// n ← n + Normalize(Normal(v, j))
-				var A = 3*index[j  ];
-				var B = 3*index[j+1];
-				var C = 3*index[j+2];
-				
-				var Ax = position[A  ];
-				var Ay = position[A+1];
-				var Az = position[A+2];
+		for (var i=0; i < normal.length; i++)
+			normal[i] = 0;
 
-				var Bx = position[B  ];
-				var By = position[B+1];
-				var Bz = position[B+2];
 
-				var Cx = position[C  ];
-				var Cy = position[C+1];
-				var Cz = position[C+2];
-				
-				var Ux = Bx - Ax;
-				var Uy = By - Ay;
-				var Uz = Bz - Az;
-				
-				var Vx = Cx - Ax;
-				var Vy = Cy - Ay;
-				var Vz = Cz - Az;
-				
-				var S = [
-					Uy*Vz - Uz*Vy,
-					Uz*Vx - Ux*Vz,
-					Ux*Vy - Uy*Vx
-				];
-				
-				// normalize
-				var l = Math.sqrt(S[0]*S[0]+S[1]*S[1]+S[2]*S[2]);
-				for (var k=0; k<3; k++) n[k] += S[k] / l;
-			}
+		// for each triangle j that shares ith vertex {
+		for (var j=0; j < il; j+=3) {
+			// if ((index[j  ] != i) && (index[j+1] != i) && (index[j+2] != i))
+				// continue;
+			
+			// n ← n + Normalize(Normal(v, j))
+			var A = 3*index[j  ];
+			var B = 3*index[j+1];
+			var C = 3*index[j+2];
+			
+			var Ax = position[A  ];
+			var Ay = position[A+1];
+			var Az = position[A+2];
+
+			var Bx = position[B  ];
+			var By = position[B+1];
+			var Bz = position[B+2];
+
+			var Cx = position[C  ];
+			var Cy = position[C+1];
+			var Cz = position[C+2];
+			
+			var Ux = Bx - Ax;
+			var Uy = By - Ay;
+			var Uz = Bz - Az;
+			
+			var Vx = Cx - Ax;
+			var Vy = Cy - Ay;
+			var Vz = Cz - Az;
+			
+			var S = [
+				Uy*Vz - Uz*Vy,
+				Uz*Vx - Ux*Vz,
+				Ux*Vy - Uy*Vx
+			];
+			
+			// normalize
+			// var l = Math.sqrt(S[0]*S[0]+S[1]*S[1]+S[2]*S[2]);
+			for (var k=0; k<3; k++) {
+				// var n = S[k] / l;
+				var n = S[k];
+				normal[A+k] += n;
+				normal[B+k] += n;
+				normal[C+k] += n;
+			};
+		}
 			
 			// v[i].n ← Normalize(n)
-			var l = Math.sqrt(n[0]*n[0]+n[1]*n[1]+n[2]*n[2]);
-			for (var k=0; k<3; k++) normal[k] = n[k] / l;
-		}
-		
-		// for(var i=0; i < info.iterateCount; i++) {
-			// normal[3*i  ] = 0;
-			// normal[3*i+1] = 1;
-			// normal[3*i+2] = 0;
+			// var l = Math.sqrt(n[0]*n[0]+n[1]*n[1]+n[2]*n[2]);
+			// for (var k=0; k<3; k++) normal[k] = n[k] / l;
 		// }
-		console.log(normal);
+		
+		for (var i=0; i < normal.length; i+=3) {
+			var l = Math.sqrt(normal[i]*normal[i]+normal[i+1]*normal[i+1]+normal[i+2]*normal[i+2]);
+			normal[i  ] = normal[i  ] / l;
+			normal[i+1] = normal[i+1] / l;
+			normal[i+2] = normal[i+2] / l;
+		}
+		// console.log(normal);
 	}
 });
 
@@ -191,17 +198,31 @@ Xflow.registerOperator("xflow.mygrid", {
 
         // Create Indices for triangles
 		var tl = (s-1) * (t-1);
+		var offset = 0;
 		for(var i = 0; i < tl; i++) {
-			var offset = i*6;
 			var base = i + Math.floor(i / (s-1));
-			index[offset+0] = base;
-			index[offset+1] = base + 1;
-			index[offset+2] = base + s;
-			index[offset+4] = base + s;
-			index[offset+3] = base + 1;
-			index[offset+5] = base + s + 1;
+			index[offset++] = base + 1;
+			index[offset++] = base;
+			index[offset++] = base + s;
+			index[offset++] = base + s;
+			index[offset++] = base + s + 1;
+			index[offset++] = base + 1;
 		}
-		
+		// var tl = (s-1) * (t-1);
+		// var offset = 0;
+		// for(var i = 0; i < t-1; i++) {
+			// for(var j = 0; j < s-1; j++) {
+				// var base = i*s + j;
+				// index[offset++] = base + s;
+				// index[offset++] = base;
+				// index[offset++] = base + 1;
+				// index[offset++] = base + 1;
+				// index[offset++] = base + s + 1;
+				// index[offset++] = base + s;
+			// }
+		// }
+		// console.log(offset);
+		// console.log(tl);
 		// Create Indices for trianglestrips
 		// var i = 0
 		// for (var row=0; row<t-1; row++) {
